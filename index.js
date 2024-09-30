84e88bc09402dbd7840953ae18e6c43c83b5c6f4
Veneridze <viacheslav.kopaev@yandex.ru>
2024-09-30 20:36:29 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+import AllowNotifications from "./Components/AllowNotifications.vue";
+import DocumentButton from "./Components/DocumentButton.vue";
+import VerifyEmail from "./Components/VerifyEmail.vue";
+import printDoc from "./Plugins/printDoc";
+
+export default {AllowNotifications, DocumentButton, VerifyEmail, printDoc}
