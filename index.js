927597277423aec3592e7c161b0144dad8d535e2
Veneridze <viacheslav.kopaev@yandex.ru>
2024-10-08 22:45:41 +0300

    Update

--- index.js
+++ index.js
@@ -4,5 +4,4 @@ import DocumentButton from "./src/Components/DocumentButton.vue";
 import VerifyEmail from "./src/Components/VerifyEmail.vue";
 import printDoc from "./src/Plugins/printDoc";
 
-module.exports = {AllowNotifications, DocumentButton, VerifyEmail, printDoc};
 export default {AllowNotifications, DocumentButton, VerifyEmail, printDoc};
